7f3cf21ef1178f5911aa41fadc57e64d4cef17f5
kkaushik14 <kumarkaushik871@gmail.com>
2025-08-08 15:49:26 +0530

    Add Login and Profile components with and context api integration

--- minicontext/src/App.jsx
+++ minicontext/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
 
   return (
     <UserContextProvider>
+      <Login />
+      <Profile />
     </UserContextProvider>
   )
 }
