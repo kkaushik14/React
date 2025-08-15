e7200dbad9dd35e07d14ff5cf39b76220de6f149
kkaushik14 <kumarkaushik871@gmail.com>
2025-08-15 20:33:48 +0530

    try fixing bug, but still there

--- todoWithReduxToolkit/src/App.jsx
+++ todoWithReduxToolkit/src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Todos from './components/Todos'
 import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-    <AddTodo />
-    <Todos />
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
